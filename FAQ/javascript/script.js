a66a5ae5a090eeb9e448d57d473c3cf57a0bd139
AMAN SHARMA <74008888+Aman2907@users.noreply.github.com>
2022-10-09 18:24:48 +0530

    Add files via upload

---
+++ FAQ/javascript/script.js
@@ -0,0 +1,258 @@
+
+
+$(document).ready(function () {
+
+  
+  $("#hola").click(function () {
+    $("#tori").show();
+    $("#chocho").hide();
+    $("#banana").hide();
+    $("#kaka").hide();
+    $("#zoo").hide();
+
+  });
+});
+
+
+
+
+
+
+
+
+//Top Question(TOP==Left)
+$(document).ready(function () {
+
+  $("#bebe").hide();
+  $("#hola").click(function () {
+    $("#bebe").show("slow");
+    $("#bebe").hide();
+  });
+});
+
+//Top Question(Right)
+$(document).ready(function () {
+  $("#tori").hide();
+  $("#hola").click(function () {
+    $("#tori").show("fast");
+    $("#tori").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#farukh").hide();
+  $("#sudhir").click(function () {
+  $("#farukh").show("slow");
+  $("#farukh").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#chalsa").hide();
+  $("#raman").click(function () {
+  $("#chalsa").show("slow");
+  $("#chalsa").hide();
+  });
+});
+
+
+
+
+
+// Shipping, Order Tracking & Delivery(top==LEFT)
+
+$(document).ready(function () {
+  $("#chocho").hide();
+  $("#qpr").click(function () {
+    $("#chocho").show("slow");
+    $("#chocho").hide();
+  });
+});
+
+
+//Shipping, Order Tracking & Delivery(right)
+$(document).ready(function () {
+  $("#hoor").hide();
+  $("#qpr").click(function () {
+    $("#hoor").show("slow");
+    $("#hoor").hide();
+  });
+});
+
+
+$(document).ready(function () {
+  $("#jerry").hide();
+  $("#koi").click(function () {
+  $("#jerry").show("slow");
+  $("#jerry").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#tom").hide();
+  $("#boi").click(function () {
+  $("#tom").show("slow");
+  $("#tom").hide();
+  });
+});
+
+
+
+
+// Cancellations and Modifications(top=left)
+
+$(document).ready(function () {
+  $("#banana").hide();
+  $("#hen").click(function () {
+    $("#banana").show("fast");
+    $("#banana").hide();
+  });
+});
+
+
+
+
+// Cancellations and Modifications(right)
+
+$(document).ready(function () {
+  $("#yuvi").hide();
+  $("#hen").click(function () {
+    $("#yuvi").show("fast");
+    $("#yuvi").hide();
+  });
+});
+
+
+
+
+
+$(document).ready(function () {
+  $("#kante").hide();
+  $("#moi").click(function () {
+    $("#kante").show("fast");
+    $("#kante").hide();
+  });
+});
+
+
+$(document).ready(function () {
+  $("#var").hide();
+  $("#zoi").click(function () {
+    $("#var").show("fast");
+    $("#var").hide();
+  });
+});
+
+
+
+
+
+
+// Returns and Exchange(right)
+
+$(document).ready(function () {
+  $("#moor").hide();
+  $("#mango").click(function () {
+    $("#moor").show("fast");
+    $("#moor").hide();
+  });
+});
+
+
+
+// // Returns and Exchange(left=top)
+$(document).ready(function () {
+  $("#kaka").hide();
+  $("#mango").click(function () {
+    $("#kaka").show("fast");
+    $("#kaka").hide();
+  });
+});
+
+
+$(document).ready(function () {
+  $("#jaysur").hide();
+  $("#aoi").click(function () {
+    $("#jaysur").show("fast");
+    $("#jaysur").hide();
+  });
+});
+
+
+$(document).ready(function () {
+  $("#dinesh").hide();
+  $("#roi").click(function () {
+    $("#dinesh").show("fast");
+    $("#dinesh").hide();
+  });
+});
+
+
+
+
+
+
+//PAyments(right)
+
+$(document).ready(function () {
+  $("#laxman").hide();
+  $("#butter").click(function () {
+    $("#laxman").show("fast");
+    $("#laxman").hide();
+  });
+});
+
+//top=left
+$(document).ready(function () {
+  $("#zoo").hide();
+  $("#butter").click(function () {
+    $("#zoo").show("fast");
+    $("#zoo").hide();
+  });
+});
+
+
+
+//subquestion
+
+$(document).ready(function () {
+  $("#sachin").hide();
+  $("#nambi").click(function () {
+    $("#sachin").show("fast");
+    $("#sachin").hide();
+  });
+});
+
+
+
+$(document).ready(function () {
+  $("#dravid").hide();
+  $("#kalyan").click(function () {
+    $("#dravid").show("fast");
+    $("#dravid").hide();
+  });
+});
+
+
+// last two
+
+$(document).ready(function () {
+  $("#ojha").hide();
+  $("#kambi").click(function () {
+    $("#ojha").show("fast");
+    $("#ojha").hide();
+  });
+});
+
+
+$(document).ready(function () {
+  $("#bumbum").hide();
+  $("#bambi").click(function () {
+    $("#bumbum").show("fast");
+    $("#bumbum").hide();
+  });
+});
+
+
+
+
